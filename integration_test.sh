87bc8242156698143e278089d8c0705bacb26949
Michael Grischenko <robotcrimefighter@gmail.com>
2018-05-23 14:48:15 +0300

    Installer script fixes and improvements
    * Removed self update functionality
    * Removed GS app id fetching - user will have to specify manually
    * Incorporated GS fix for unicode pbxproj files
    * Better check for framework being added to project
    * Site ID and API Key are now added to info.plist

--- integration_test.sh
+++ integration_test.sh
@@ -30,7 +30,7 @@ rm -rf "$PROJECT_DIR"
 unzip "$DIR/demo.zip" -d "$DIR"
 cp "$DIR/talkable.sh" "$PROJECT_DIR/talkable.sh"
 cd "$PROJECT_DIR" || exit 1
-bash "$PROJECT_DIR/talkable.sh" -s=badger -k=badger --debug=true
+bash "$PROJECT_DIR/talkable.sh" -s=badger -k=mushroom -g=F8899676O7EZt --debug=true
 
 exists "$PROJECT_DIR/getsocial-installer-script-*/installer.py" || fatal "GetSocial installer script was not downloaded"
 [ -d "$PROJECT_DIR/TalkableSDK/TalkableSDK.framework" ] || fatal "TalkableSDK was not downloaded"
@@ -45,6 +45,10 @@ $PLIST_BUDDY -c "Print :CFBundleURLTypes" "$PROJECT_DIR/$INFOPLIST_FILE" | inclu
 
 $PLIST_BUDDY -c "Print :LSApplicationQueriesSchemes" "$PROJECT_DIR/$INFOPLIST_FILE" | includes "tkbl-" || fatal "Talkable Query Scheme was not added"
 $PLIST_BUDDY -c "Print :LSApplicationQueriesSchemes" "$PROJECT_DIR/$INFOPLIST_FILE" | includes "kakaokompassauth" || fatal "GetSocial Query Schemes were not added"
+
+$PLIST_BUDDY -c "Print :com.talkable.ios-sdk.site_slug" "$PROJECT_DIR/$INFOPLIST_FILE" | includes "badger" || fatal "Talkable Site ID was not set"
+$PLIST_BUDDY -c "Print :com.talkable.ios-sdk.api_key" "$PROJECT_DIR/$INFOPLIST_FILE" | includes "mushroom" || fatal "Talkable API Key not set"
+$PLIST_BUDDY -c "Print :im.getsocial.sdk.AppId" "$PROJECT_DIR/$INFOPLIST_FILE" | includes "F8899676O7EZt" || fatal "GetSocial App ID not set"
 
 cd "$PROJECT_DIR" && xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO || fatal "Project could not be built"
 
--- talkable.sh
+++ talkable.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-ARGUMENTS="$*"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCRIPT_PATH="$DIR/$( basename "${BASH_SOURCE[0]}" )"
-VERSION="0.0.2"
 DEBUG=false
 
 PYTHON="python"
@@ -18,17 +14,16 @@ SITE_ID=""
 API_KEY=""
 GETSOCIAL_APP_ID=""
 FRAMEWORK_VERSION=""
-GETSOCIAL_VERSION=""
-DEFAULT_GETSOCIAL_VERSION="0.0.9"
 
 FRAMEWORK_NAME="TalkableSDK"
 FRAMEWORK_DIR="$PROJECT_DIR/$FRAMEWORK_NAME"
 FRAMEWORK_PATH="$FRAMEWORK_DIR/$FRAMEWORK_NAME.framework"
 FRAMEWORK_PLIST_PATH="$FRAMEWORK_PATH/Info.plist"
-TALKABLE_VERSION_URL="https://human-spider.github.io/index.html"
 GETSOCIAL_VERSION_URL="https://downloads.getsocial.im/ios-installer/releases/latest.json"
 FRAMEWORK_BUNDLE_ID='com.talkable.ios-sdk'
-GETSOCIAL_PARAMS="--use-ui false --ignore-cocoapods true"
+INFOPLIST_KEY_SITE_ID="$FRAMEWORK_BUNDLE_ID.site_slug"
+INFOPLIST_KEY_API_KEY="$FRAMEWORK_BUNDLE_ID.api_key"
+GETSOCIAL_PARAMS="--use-ui=false --ignore-cocoapods=true"
 
 # Helper Functions
 
@@ -82,41 +77,10 @@ downloadAndUnzip() {
 
 # Actions
 
-selfUpdate() {
-  if [ -n "$REQUIRE_VERSION" ] && [ "$REQUIRE_VERSION" != "$VERSION" ]; then
-    # Installer script was updated and relaunched, but the version is still not the desired version.
-    # Don't launch self update again so we don't enter an infinite loop.
-    warn "Installer could not be updated to version $REQUIRE_VERSION, is at version $VERSION instead"
-    return 0
-  fi
-
-  local required_version
-  local download_url
-
-  required_version=$(echo "$TALKABLE_DATA" | getJSONValue "installer_version")
-  if [ -z "$required_version" ] || [ "$VERSION" = "$required_version" ]; then
-    verbose "Current installer version $VERSION satisfies requirement"
-  else
-    verbose "Updating installer script to version $required_version"
-    download_url=$(echo "$TALKABLE_DATA" | getJSONValue "installer_url")
-    if [ -z "$download_url" ]; then
-      warn "Could not fetch URL to download installer update"
-      return 0
-    fi
-    downloadFile "$SCRIPT_PATH" "$download_url"
-    chmod +x "$SCRIPT_PATH"
-    /bin/bash "$SCRIPT_PATH $ARGUMENTS --require-version=$required_version"
-    exit 0
-  fi
-}
-
 downloadTalkableFramework() {
   local framework_download_needed=true
   local framework_current_version
   local download_url
-
-  # use framework version from talkable data if not specified
-  [ -z "$FRAMEWORK_VERSION" ] && FRAMEWORK_VERSION=$(echo "$TALKABLE_DATA" | getJSONValue "framework_version")
 
   # check if version was fetched correctly and get download url for that version
   # verify version with regex because it can contain error page HTML
@@ -155,7 +119,7 @@ downloadGetSocialInstaller() {
     GETSOCIAL_VERSION=$DEFAULT_GETSOCIAL_VERSION
   fi
 
-  local getsocisal_download_url="https://downloads.getsocial.im/ios-installer/releases/ios-installer-$GETSOCIAL_VERSION.zip"
+  local getsocial_download_url="https://downloads.getsocial.im/ios-installer/releases/ios-installer-$GETSOCIAL_VERSION.zip"
 
   #download und unzip GetSocial installer if needed
   GETSOCIAL_INSTALLER_DIR="$PROJECT_DIR/getsocial-installer-script-$GETSOCIAL_VERSION"
@@ -163,7 +127,7 @@ downloadGetSocialInstaller() {
   if [ ! -e "$GETSOCIAL_INSTALLER_DIR/installer.py" ]; then
     verbose "Downloading GetSocial installer script..."
     rm -rf "$PROJECT_DIR"/getsocial-installer-script-*
-    downloadAndUnzip "$getsocisal_download_url" "$PROJECT_DIR/getsocial-installer-script.zip" "$GETSOCIAL_INSTALLER_DIR"
+    downloadAndUnzip "$getsocial_download_url" "$PROJECT_DIR/getsocial-installer-script.zip" "$GETSOCIAL_INSTALLER_DIR"
   else
     verbose "GetSocial installer script v$GETSOCIAL_VERSION already downloaded"
   fi
@@ -174,13 +138,16 @@ addTalkableFrameworkToProject() {
   [ ! -f "$FRAMEWORK_PLIST_PATH" ] && fatal "Talkable SDK could not be downloaded"
   [ ! -e "$GETSOCIAL_INSTALLER_DIR/installer.py" ] && fatal "GetSocial installer script could not be downloaded"
 
-  if getPlistValue "$PROJECT_FILE_PATH/project.pbxproj" | includes "$FRAMEWORK_NAME"; then
+  if getPlistValue "$PROJECT_FILE_PATH/project.pbxproj" | includes "$FRAMEWORK_NAME.framework"; then
     verbose "SDK already added to $PROJECT_FILE_PATH"
     return 0
   fi
 
   PYTHON_SCRIPT="
 import sys
+if sys.version_info < (3, 0):
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
 sys.path.insert(0, \"$GETSOCIAL_INSTALLER_DIR\")
 sys.path.insert(0, \"$GETSOCIAL_INSTALLER_DIR/openstep_parser\")
 sys.path.insert(0, \"$GETSOCIAL_INSTALLER_DIR/pbxproj\")
@@ -198,7 +165,7 @@ if len(added_files) > 0: project.save()
   #add Talkable SDK to project
   verbose "Adding SDK to XCode Project file: $PROJECT_FILE_PATH"
   $PYTHON -c "$PYTHON_SCRIPT"
-  if getPlistValue "$PROJECT_FILE_PATH/project.pbxproj" | includes "$FRAMEWORK_NAME"; then
+  if getPlistValue "$PROJECT_FILE_PATH/project.pbxproj" | includes "$FRAMEWORK_NAME.framework"; then
     verbose "SDK added to $PROJECT_FILE_PATH"
   else
     fatal "SDK could not be added to $PROJECT_FILE_PATH"
@@ -208,6 +175,20 @@ if len(added_files) > 0: project.save()
 configureInfoPlist() {
   local url_scheme_name="tkbl-$SITE_ID"
   local infoplist_full_path="$PROJECT_DIR/$INFOPLIST_FILE"
+
+  if [ "$(getPlistValue "$infoplist_full_path" "$INFOPLIST_KEY_SITE_ID")" != "$SITE_ID" ]; then
+    plistBuddyExec "$infoplist_full_path" << EOF
+Add :$INFOPLIST_KEY_SITE_ID string $SITE_ID
+Set :$INFOPLIST_KEY_SITE_ID $SITE_ID
+EOF
+  fi
+
+  if [ "$(getPlistValue "$infoplist_full_path" "$INFOPLIST_KEY_API_KEY")" != "$API_KEY" ]; then
+    plistBuddyExec "$infoplist_full_path" << EOF
+Add :$INFOPLIST_KEY_API_KEY string $API_KEY
+Set :$INFOPLIST_KEY_API_KEY $API_KEY
+EOF
+  fi
 
   #add Talkable URL Scheme to info.plist
   if getPlistValue "$infoplist_full_path" "CFBundleURLTypes" | includes "$url_scheme_name"; then
@@ -262,9 +243,6 @@ while [ "$1" != "" ]; do
       --getsocial-app-id | -g)
           GETSOCIAL_APP_ID=$VALUE
           ;;
-      --require-version)
-          REQUIRE_VERSION=$VALUE
-          ;;
       --debug)
           DEBUG=true
           ;;
@@ -277,17 +255,9 @@ done
 
 [ -z "$SITE_ID" ] && fatal "--site-id param is mandatory"
 [ -z "$API_KEY" ] && fatal "--api-key param is mandatory"
-
-# Fetch version data and GetSocial App ID from Talkable
-
-TALKABLE_DATA=$(curl -s -X GET "$TALKABLE_VERSION_URL") #this request will call Talkable API and contain site ID and API key
-
-[ -z "$GETSOCIAL_APP_ID" ] && GETSOCIAL_APP_ID=$(echo "$TALKABLE_DATA" | getJSONValue "getsocial_app_id")
-[ -z "$GETSOCIAL_APP_ID" ] && fatal "Could not fetch GetSocial App ID"
+[ -z "$GETSOCIAL_APP_ID" ] && fatal "--getsocial-app-id param is mandatory"
 
 verbose "Site ID: $SITE_ID, API Key: $API_KEY, GetSocial App ID: $GETSOCIAL_APP_ID"
-
-selfUpdate
 
 # Perform the installation
 
